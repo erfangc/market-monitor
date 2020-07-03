e033968055197c298596deb69391d09ffb2fef85
erfangchen <erfangc@gmail.com>
2020-07-03 18:18:06 -0400

    wip

--- market-monitor-api/src/main/kotlin/com/github/erfangc/marketmonitor/io/ApiKeys.kt
+++ market-monitor-api/src/main/kotlin/com/github/erfangc/marketmonitor/io/ApiKeys.kt
@@ -1,7 +1,6 @@
 package com.github.erfangc.marketmonitor.io
 
 object ApiKeys {
-    val quandlApiKey = System.getenv("QUANDL_API_KEY") ?: error("QUANDL_API_KEY missing from environment")
-    val alphaVantageApiKey = System.getenv("ALPHA_VANTAGE_API_KEY")
-            ?: error("environment ALPHA_VANTAGE_API_KEY is not defined")
+    val quandlApiKey = System.getenv("QUANDL_API_KEY")
+            ?: error("QUANDL_API_KEY missing from environment")
 }
