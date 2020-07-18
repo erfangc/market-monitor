703cf558f236856ca308fa58e4c3f2105ffc11f2
erfangchen <erfangc@gmail.com>
2020-07-18 08:49:20 -0400

    small fix

--- market-monitor-api/src/main/kotlin/com/github/erfangc/marketmonitor/dailymetrics/DailyMetricsService.kt
+++ market-monitor-api/src/main/kotlin/com/github/erfangc/marketmonitor/dailymetrics/DailyMetricsService.kt
@@ -1,26 +1,20 @@
 package com.github.erfangc.marketmonitor.dailymetrics
 
-import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.erfangc.marketmonitor.dailymetrics.models.DailyMetric
 import com.github.erfangc.marketmonitor.dailymetrics.models.DailyMetricRow
-import com.github.erfangc.marketmonitor.io.HttpClient
 import com.github.erfangc.marketmonitor.io.MongoDB.database
+import com.github.erfangc.marketmonitor.quandl.QuandlService.exportQuandl
 import com.mongodb.client.model.Indexes
 import com.vhl.blackmo.grass.dsl.grass
-import org.apache.http.client.methods.HttpGet
 import org.litote.kmongo.*
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import java.io.InputStream
 import java.time.LocalDate
 
 @ExperimentalStdlibApi
 @Service
 class DailyMetricsService {
     private val log = LoggerFactory.getLogger(DailyMetricsService::class.java)
-
-    private val apiKey = System.getenv("QUANDL_API_KEY")
-            ?: error("environment QUANDL_API_KEY is not defined")
 
     companion object {
         val dailyMetrics = database.getCollection<DailyMetricRow>()
@@ -69,25 +63,18 @@ class DailyMetricsService {
         }
     }
 
-
     fun loadForDate(date: LocalDate) {
-        val rows = csvReader().readAllWithHeader(csvInputStream(date))
-        if (rows.isNotEmpty()) {
-            val documents = grass<DailyMetric>().harvest(rows)
-                    .map { DailyMetricRow(_id = "${it.ticker}:${it.date}", dailyMetric = it) }
-            val result = dailyMetrics.insertMany(documents)
-            log.info("Wrote ${result.insertedIds.size} daily metrics for $date")
-        } else {
-            log.info("Skipping writing daily metrics for $date since there are no results dayOfWeek=${date.dayOfWeek}")
+        exportQuandl(publisher = "SHARADAR", table = "DAILY") { response ->
+            val rows = response.asList()
+            if (rows.isNotEmpty()) {
+                val documents = grass<DailyMetric>().harvest(rows)
+                        .map { DailyMetricRow(_id = "${it.ticker}:${it.date}", dailyMetric = it) }
+                val result = dailyMetrics.insertMany(documents)
+                log.info("Wrote ${result.insertedIds.size} daily metrics for $date")
+            } else {
+                log.info("Skipping writing daily metrics for $date since there are no results dayOfWeek=${date.dayOfWeek}")
+            }
         }
     }
-
-    private fun csvInputStream(date: LocalDate): InputStream = HttpClient.httpClient
-            .execute(HttpGet(url(date)))
-            .entity
-            .content
-
-    private fun url(date: LocalDate) =
-            "https://www.quandl.com/api/v3/datatables/SHARADAR/DAILY.csv?date=${date}&api_key=$apiKey"
 
 }
